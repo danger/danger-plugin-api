c769a1d3d07f58832146ec828ddb654e993c6f5d
Orta Therox <orta.therox@gmail.com>
2016-08-25 17:27:12 -0400

    init

---
+++ lib/danger/plugin/api/version.rb
@@ -0,0 +1,7 @@
+module Danger
+  module Plugin
+    module Api
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ spec/danger/plugin/api_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Danger::Plugin::Api do
+  it 'has a version number' do
+    expect(Danger::Plugin::Api::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
